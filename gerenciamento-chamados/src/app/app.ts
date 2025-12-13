4e986fa93c6bafd315807ebae399696bc29e9319
Juliana Valle <juju.pvg@gmail.com>
2025-12-13 20:00:03 -0300

    feat: adicionando esqueleto do botão de novo chamado

--- gerenciamento-chamados/src/app/app.ts
+++ gerenciamento-chamados/src/app/app.ts
@@ -1,13 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CardChamadosComponent } from './chamados/components/card-chamados/card-chamados.component';
+import { ButtonComponent } from "./shared/components/button/button.component";
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
   imports: [
     CardChamadosComponent,
-  ],
+    ButtonComponent
+],
 })
 
 export class App {
